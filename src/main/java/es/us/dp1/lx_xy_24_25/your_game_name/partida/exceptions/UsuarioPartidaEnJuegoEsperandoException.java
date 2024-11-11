8bbaa8340d555d13d6c738631a0b9d0399b24740
CRISTINA FERNANDEZ <correoparacristina100@gmail.com>
2024-11-11 02:08:57 +0100

    Excepción mínimo jugadores para iniciar una partida

--- src/main/java/es/us/dp1/lx_xy_24_25/your_game_name/partida/exceptions/UsuarioPartidaEnJuegoEsperandoException.java
+++
@@ -1,9 +0,0 @@
-package es.us.dp1.lx_xy_24_25.your_game_name.partida.exceptions;
-
-public class UsuarioPartidaEnJuegoEsperandoException extends RuntimeException{
-
-    public UsuarioPartidaEnJuegoEsperandoException(String message) {
-        super(message);
-    }
-
-}

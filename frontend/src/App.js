92a86fc98d837f81d096d2f964acb4587d514a39
MARTA AGUILAR <marta.aguilar.morcillo1@gmail.com>
2024-11-29 01:19:42 +0100

    Añadir de nuevo clase intento modularización de componente Jugando()

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -18,6 +18,7 @@ import Instructions from "./player/instrucciones";
 import PartidaListAdmin from "./admin/partidas/PartidaListAdmin";
 import Jugando from "./play/jugando";
 import UserStatisticsDashboard from "./admin/users/UserDashboard";
+import Jugando2 from "./play/jugando2";
 
 function ErrorFallback({ error, resetErrorBoundary }) {
   return (
@@ -62,6 +63,7 @@ function App() {
           <Route path="/play" exact={true} element={<PrivateRoute><Play/></PrivateRoute>} />
           <Route path="/salaEspera/:partidaId" exact={true} element={<PrivateRoute><SalaEspera/></PrivateRoute>} />
           <Route path="/tablero/:partidaId" exact={true} element={<PrivateRoute><Jugando/></PrivateRoute>} />
+          <Route path="/tablero2/:partidaId" exact={true} element={<PrivateRoute><Jugando2/></PrivateRoute>} />
         </>)
     }    
   })
